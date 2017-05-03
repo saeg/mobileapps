00eb0b465c0f33a27c10e5bccdaf33b43050d9b2
Marcos Lordello Chaim <marcoschaim@bitbucket.org>
2017-05-03 09:42:14 +0200

    zxing for SAT assessment

---
+++ zxing-master/zxing.appspot.com/target/.generated/com/google/gwt/lang/com_00046google_00046zxing_00046web_00046generator_00046Generator__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046google_00046zxing_00046web_00046generator_00046Generator__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
